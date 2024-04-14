041f01defff4dd18d0c61588372bb2875cedd946
zh-zheng <qishuangyiren@outlook.com>
2024-04-14 18:26:42 +0800

    add notion css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
 import { Analytics } from '@vercel/analytics/react'
+import "../assets/css/notion.css"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
