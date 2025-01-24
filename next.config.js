6297aed6fc6805a8d7e4be18f582d662abad0ce7
zh-zheng <qishuangyiren@outlook.com>
2025-01-24 14:12:14 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: [
+      'www.notion.so', 
+      'lh5.googleusercontent.com', 
+      's3-us-west-2.amazonaws.com',
+      'prod-files-secure.s3.us-west-2.amazonaws.com'
+    ],
   },
 }
